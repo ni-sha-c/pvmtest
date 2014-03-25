7a5829f547fadfa057cccaaa69475f96611402db
nishaChandramoorthy <nishac92@gmail.com>
2014-03-25 19:55:36 +0530

    finally creating a disk. not sure if that's what I should do.

--- thinringcreate.c
+++ thinringcreate.c
@@ -9,17 +9,19 @@ int main()
 
 	FILE *inputfile;
 	NODE *node;
-	UINT i,N;
-	REAL r_i, z_i, theta, dtheta;
+	UINT i,N_bound,N;
+	REAL r_bound, z_i, r_i, theta, dtheta;
+	REAL gamma, dgamma;	
 
 	N = 100;
-	r_i = 0.5e0;
+	r_bound = 1.0e0;
 	z_i = 0.5e0;	
 	dtheta = 2*pi/N;
+	dgamma = 1.e0/
 	node = (NODE *) calloc(N,sizeof(NODE));
 	
-	inputfile = fopen("posalpha_thinring.dat", "w");
-	theta = -dtheta;
+	inputfile = fopen("posalpha_thinring_b.dat", "w");
+	theta = -dtheta - 0.06;
 	for(i=0;i<N;i++)
 	{
 		theta = theta + dtheta;
