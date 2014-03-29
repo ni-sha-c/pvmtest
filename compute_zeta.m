2befc13596f9895f3e335f820a7a9b77e98a4edb
nishaChandramoorthy <nishac92@gmail.com>
2014-03-29 19:30:30 +0530

    have to verify discretization of norbury ring.

--- compute_zeta.m
+++ compute_zeta.m
@@ -1,7 +1,6 @@
-function res = compute_zeta(x) 
-	delta = 5.e-2;
+function res = compute_zeta(x,delta) 
 	y = x/delta;
-	c1 = 7.5e0/4.e0/pi/delta^3;
+	c1 = 7.5e0/4.e0/pi/(delta^3);
 	den = (1 + y.*y).^(3.5e0);
 	res = c1./den;
 endfunction
--- diskcreate.m
+++ diskcreate.m
@@ -1,11 +1,13 @@
-	Omega = ;
-	N_bound = 130;
+	Omega = 0.5e0; %stands for the Omega from Norbury's paper
+	N_bound = 120;
 	r_i = 0.5e0;
 	z_i = 0.5e0;
-	sigma = 0.05e0;
-	N_r = 6;
-	r_l = sigma/(2.e0*N_r + 1.e0);	
+	sigma = 0.275e0;
+	N_r = 3;
+	r_l = sigma/(2.e0*N_r + 1.e0);
+	delta = 0.155;	
 	N_1 = 6;
+	N_per_theta = 1 + N_1*N_r*(N_r + 1)/2;
 	theta = linspace(0.e0, 2*pi, N_bound);
 	
 	ct = cos(theta);
@@ -15,13 +17,48 @@
 	Y_c = r_i*st;
 	Z_c = z_i*ones(1,N_bound);		
 	
-	r = linspace(0.e0,2.e0*r_l,N_r);
-	w_x =  Omega*r.*st; 
-	w_y = -Omega*r.*ct;
+	%r = linspace(0.e0,N_r*2.e0*r_l,N_r+1);
+	%w_x =  Omega*r.*st; 
+	%w_y = -Omega*r.*ct;
 	
-	i = 1:N_r
-	N_theta = [1, 6*i];
-	dphi_i = 2*pi/N_theta;
+	i = 1:N_r;
+	N_theta = [1, N_1*i];
+	
+	l = 2;
+	count = 0;
+	outline1(1) = 0;
+	outline2(1) = 0;	
+	for k = 2:N_per_theta
+		if(mod(k,6)==2 && k > sum(N_theta(1:l),2))
+			l = l + 1;
+			count = 0;
+		end 
+		outline1(k) = l-1;
+		outline2(k) = 1.e0/(l-1)*count;
+		count = count + 1;
+	end
 		
+	r_c = repmat(2.e0*r_l*outline1, N_per_theta,1);
+	r_ct= r_c';
+	phi = repmat(pi/N_1*outline2, N_per_theta, 1);
+	phi_t = phi';
+	w = 2.e0*r_l*Omega*outline1;
+
+	magxmxi = r_c.*r_c + r_ct.*r_ct - 2.e0*(r_c.*r_ct).*(cos(phi-phi_t));
+	F_delta = compute_zeta(magxmxi,delta);
+	alpha = F_delta\w';
+
+	cp = cos(pi/N_1*outline2);
+	sp = sin(pi/N_l*outline2);
+	for j = 1:N_bound
+		xc = repmat(X_c(j), N_per_theta, 1);
+		yc = repmat(Y_c(j), N_per_theta, 1);
+		zc = repmat(Z_c(j), N_per_theta, 1);
+	  		
+
+
+				
+			
 		
+		 	
 			
